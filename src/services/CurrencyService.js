313d0d26e0407da5ff665c2a5a129be2b0458859
Felipe De la Fuente V <fadelafuente1@gmail.com>
2018-11-08 03:09:00 -0300

    change params to baseCurrency

--- src/services/CurrencyService.js
+++ src/services/CurrencyService.js
@@ -1,7 +1,7 @@
 import Api from './Api';
 
 export default {
-  getCurrency (params) {
-    return Api().get(`/latest?base=${params.baseCurrency}`)
+  getCurrency (baseCurrency) {
+    return Api().get(`/latest?base=${baseCurrency}`)
   }
 }
