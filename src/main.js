52733ddcc20be2e7b89088500474239d81d192b9
Felipe De la Fuente V <fadelafuente1@gmail.com>
2018-11-11 16:24:21 -0300

    add material components

--- src/main.js
+++ src/main.js
@@ -7,12 +7,17 @@ import BootstrapVue from 'bootstrap-vue'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'bootstrap-vue/dist/bootstrap-vue.css'
 import store from './store'
-import { MdTable, MdContent, MdCard } from 'vue-material/dist/components'
+import { MdTable, MdContent, MdCard, MdButton, MdIcon, MdDrawer, MdToolbar, MdList } from 'vue-material/dist/components'
 import 'vue-material/dist/vue-material.min.css'
 
 Vue.use(MdTable)
 Vue.use(MdContent)
 Vue.use(MdCard)
+Vue.use(MdButton)
+Vue.use(MdIcon)
+Vue.use(MdDrawer)
+Vue.use(MdToolbar)
+Vue.use(MdList)
 
 Vue.config.productionTip = false
 Vue.use(BootstrapVue)
